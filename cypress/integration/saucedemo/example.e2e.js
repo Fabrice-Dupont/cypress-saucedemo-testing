50b729ef1175b3ba5c05d5570f412e187588dd13
pramam <57686495+pramam@users.noreply.github.com>
2022-02-21 20:59:38 -0800

    Loading fixtures without using the this keyword; async/await

--- cypress/integration/saucedemo/example.e2e.js
+++ cypress/integration/saucedemo/example.e2e.js
@@ -1,17 +1,17 @@
 /// <reference types="cypress" />
 
-describe('UserStory: Login', () => {
-    before(function () {
-        cy.fixture('logindata.json').then(function (data){
-            this.loginData = data;
-        })
-    })
-    it(`Login_1: Should be able to login with valid credentials`, function(){
-        cy.login(this.loginData.userName, this.loginData.password);
-        cy.ensureOnInventoryPage();
-    })    
-    it(`Login_2: Logout after login`, ()=>{
-        cy.logout();
-    })
-});
+describe("UserStory: Login", () => {
+  let loginData;
 
+  before(async () => {
+    loginData = await cy.fixture("logindata.json");
+  });
+
+  it(`Login_1: Should be able to login with valid credentials`, function () {
+    cy.login(loginData.userName, loginData.password).ensureOnInventoryPage();
+  });
+
+  it(`Login_2: Logout after login`, () => {
+    cy.logout();
+  });
+});
