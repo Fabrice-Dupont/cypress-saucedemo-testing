e236f6f4f3caae98ab83f441c3253719b38c0a6f
pramam <57686495+pramam@users.noreply.github.com>
2022-02-25 17:59:03 -0800

    Reset_1: Reset App State from InventoryPage

---
+++ cypress/integration/saucedemo/reset1.e2e.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+describe("UserStory: Reset Cart State", () => {
+  // State machine info:
+  // X 1 A 2 2 15 A
+
+  let loginData;
+
+  before(async () => {
+    loginData = await cy.fixture("logindata.json");
+  });
+
+  it("Reset_1: Reset App State from InventoryPage", async () => {
+    cy.login(loginData.userName, loginData.password).ensureOnInventoryPage();
+
+    cy.addItemToCart("#add-to-cart-sauce-labs-backpack");
+    cy.addItemToCart("#add-to-cart-sauce-labs-bike-light");
+    cy.checkNumCartItems(2);
+    cy.resetAppState();
+
+    cy.checkNumCartItems(0);
+    // Ensure on same page
+    cy.ensureOnInventoryPage();
+  });
+});
