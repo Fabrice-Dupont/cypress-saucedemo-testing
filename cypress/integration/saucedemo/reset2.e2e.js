1b86a0ce3ce8295d98a60e960dda220dbaa3f5ef
pramam <57686495+pramam@users.noreply.github.com>
2022-02-25 18:05:14 -0800

    Reset_2: Reset App State from Cart Page

---
+++ cypress/integration/saucedemo/reset2.e2e.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+describe("UserStory: Reset Cart State", () => {
+  // State machine info:
+  // X 1 A 2 2 4 B 15 B
+
+  let loginData;
+
+  before(async () => {
+    loginData = await cy.fixture("logindata.json");
+  });
+
+  it("Reset_2: Reset App State from Cart Page", async () => {
+    cy.login(loginData.userName, loginData.password).ensureOnInventoryPage();
+
+    cy.addItemToCart("#add-to-cart-sauce-labs-backpack");
+    cy.addItemToCart("#add-to-cart-sauce-labs-bike-light");
+    cy.checkNumCartItems(2);
+
+    cy.clickOnCartIcon();
+    cy.ensureOnCartPage();
+
+    cy.resetAppState();
+
+    cy.checkNumCartItems(0);
+    // Ensure on same page
+    cy.ensureOnCartPage();
+  });
+});
