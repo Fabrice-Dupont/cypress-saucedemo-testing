d3c379cf3f9bafa32111357b74339994cdf5aef0
pramam <57686495+pramam@users.noreply.github.com>
2022-04-18 16:02:13 -0700

    LoginError_2: Removed spurious async from it block(leftover from WebdriverIO code)

--- cypress/integration/saucedemo/loginerror2.e2e.js
+++ cypress/integration/saucedemo/loginerror2.e2e.js
@@ -3,7 +3,7 @@
 describe("UserStory: Login", () => {
   // State Machine:
   // A Z
-  it("LoginError_2: accessing a valid page without being logged in should show correct Error message", async () => {
+  it("LoginError_2: accessing a valid page without being logged in should show correct Error message", () => {
     const subPath = "inventory.html";
     cy.visit("https://www.saucedemo.com/inventory.html", {
       failOnStatusCode: false, // gives a 404, we want to continue checking the error message
